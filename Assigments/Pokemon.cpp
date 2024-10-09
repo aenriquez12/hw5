eafb00a6afd7f425aae8d4edbc0944ff8c4344c5
Anthony Enriquez <tenriquez1818@outlook.com>
2024-10-09 00:15:19 -0500

    hello

---
+++ Assigments/Pokemon.cpp
@@ -0,0 +1,30 @@
+#include "pokemon.h"
+
+
+Pokemon::Pokemon(string name, int ndex, Type type1) {
+
+
+}
+
+Pokemon::Pokemon(string name, int ndex, Type type1, Type type2) {
+
+}
+
+string Pokemon::name() {
+
+}
+
+Pokemon::Type type1() {
+
+}
+
+bool Pokemon::is_multitype() {
+
+}
+
+Pokemon::Type type2() {
+
+}
+float Pokemon::damage_multiplier() {
+
+}
