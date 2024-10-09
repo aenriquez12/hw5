eef1c27b98726620dacd29115e7a016ee08c7804
aenriquez12 <156951230+aenriquez12@users.noreply.github.com>
2024-10-09 03:56:22 -0500

    pokedex.cpp only class

---
+++ pokedex.cpp
@@ -0,0 +1,27 @@
+#include "pokedex.h"
+
+Pokedex::Pokedex(){
+
+}
+Pokedex::Pokedex(string filename){
+
+}
+void Pokedex::save(string filename){
+
+}
+void Pokedex::add(Pokemon* p){
+
+}
+void pokedex::remove(Pokemon* p){
+
+}
+
+Pokemon* Pokedex::lookup_by_name(string name){
+
+}
+Pokemon* Pokedex::lookup_by_Ndex(int ndex){
+
+}
+int Pokedex::size(){
+    
+}
